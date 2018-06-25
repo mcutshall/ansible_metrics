a6d3e1babd5e2ebdc169db1fb2dcd7386fe599e1
mcutshall <cutshall@email.arizona.edu>
2018-06-25 11:30:38 -0700

    first

---
+++ profile_tasks.py
@@ -0,0 +1,124 @@
+import datetime
+import os
+import time
+import MySQLdb
+from ansible.plugins.callback import CallbackBase
+
+
+class CallbackModule(CallbackBase):
+    """
+    A plugin for timing tasks
+    """
+    def __init__(self):
+        super(CallbackModule, self).__init__()
+        self.stats = {}
+        self.current = None
+
+    def db_connect(self):
+        conn = MySQLdb.connect(host= "localhost",
+                  user="root",
+                  passwd="none",
+                  db="metrics")
+        x = conn.cursor()
+
+        print("xxxxxxxxxx connected to db xxxxxxxxxxxxxx")
+        conn.close()
+
+    def create_playbook_table(self):
+        conn = MySQLdb.connect(host= "localhost",
+                  user="root",
+                  passwd="none",
+                  db="metrics")
+        x = conn.cursor()
+        print("xxxxxxxxxx connected to db xxxxxxxxxxxxxx")
+
+        x.execute("""CREATE TABLE IF NOT EXISTS %s (
+        id int(11) NOT NULL AUTO_INCREMENT,
+        name varchar(255),
+        time_elapsed TIME,sudo usermod -aG docker
+        date DATE,
+        PRIMARY KEY (id)
+        );)""", (self.current)
+        print("xxxxxxxxxxxxxxx self.current: " + self.current)
+        x.close()
+
+    def record_task(self, stats):
+        conn = MySQLdb.connect(host= "localhost",
+                  user="root",
+                  passwd="none",
+                  db="metrics")
+        x = conn.cursor()
+
+        # Record the timing of the very last task
+        if self.current is not None:
+            self.stats[self.current] = time.time() - self.stats[self.current]
+
+        # Sort the tasks by their running time
+        results = sorted(
+            self.stats.items(),
+            key=lambda value: value[1],
+            reverse=True,
+        )
+
+        # Insert the timings
+        for name, elapsed in results:
+            x.execute("""INSERT INTO %s name, time_elapsed VALUES (%s, %s)""", (self,name, elapsed))
+
+        x.execute("""INSERT INTO %s name, time_elapsed VALUES (%s, %s)""", (self, self.name, self.elapsed))
+
+        x.close()
+
+    def playbook_on_task_start(self, name, is_conditional):
+        """
+        Logs the start of each task
+        """
+
+        if os.getenv("ANSIBLE_PROFILE_DISABLE") is not None:
+            return
+
+        if self.current is not None:
+            # Record the running time of the last executed task
+            self.stats[self.current] = time.time() - self.stats[self.current]
+
+        # Record the start time of the current task
+        self.current = name
+        self.stats[self.current] = time.time()
+
+    def playbook_on_stats(self, stats):
+        """
+        Prints the timings
+        """
+
+        if os.getenv("ANSIBLE_PROFILE_DISABLE") is not None:
+            return
+
+        # Record the timing of the very last task
+        if self.current is not None:
+            self.stats[self.current] = time.time() - self.stats[self.current]
+
+        # Sort the tasks by their running time
+        results = sorted(
+            self.stats.items(),
+            key=lambda value: value[1],
+            reverse=True,
+        )
+
+        # Just keep the top 10
+        results = results[:10]
+
+        # Print the timings
+        for name, elapsed in results:
+            print(
+                "{0:-<70}{1:->9}".format(
+                    '{0} '.format(name),
+                    ' {0:.02f}s'.format(elapsed),
+                )
+            )
+
+        total_seconds = sum([x[1] for x in self.stats.items()])
+        print("\nPlaybook finished: {0}, {1} total tasks.  {2} elapsed. \n".format(
+                time.asctime(),
+                len(self.stats.items()),
+                datetime.timedelta(seconds=(int(total_seconds)))
+                )
+          )
