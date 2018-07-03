59546a63af1d95e25da7569058e10aa8b46d6f62
mcutshall <cutshall@email.arizona.edu>
2018-07-03 11:46:25 -0700

    added update bash file

---
+++ update_changes.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo su -
+
+cd /opt/dev/atmosphere-ansible/ansible/playbooks/instance_deploy/callback_plugins
+rm -f profile_tasks.py
+wget https://raw.githubusercontent.com/mcutshall/ansible_metrics/master/profile_tasks.py
+
+cd /opt/dev/atmosphere-ansible/ansible/plugins/callback_plugins/
+rm -f profile_tasks.py
+wget https://raw.githubusercontent.com/mcutshall/ansible_metrics/master/profile_tasks.py
+chmod +x profile_tasks.py
+
+cd /opt/env/atmo_master/lib/python2.7/site-packages/subspace/plugins/callback/
+rm -f profile_tasks.py  profile_tasks.pyc  profile_tasks.pyo
+wget https://raw.githubusercontent.com/mcutshall/ansible_metrics/master/profile_tasks.py
+python -m py_compile profile_tasks.py
+
+cd /opt/dev/clank/callback_plugins
+rm -f profile_tasks.py  profile_tasks.pyc
+wget https://raw.githubusercontent.com/mcutshall/ansible_metrics/master/profile_tasks.py
+python -m py_compile profile_tasks.py
