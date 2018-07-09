8d73679ba23da6ea07966bd384e1801f04666012
mcutshall <cutshall@email.arizona.edu>
2018-07-09 13:34:49 -0700

    update_changes

--- update_changes.sh
+++ update_changes.sh
@@ -9,10 +9,10 @@ wget https://raw.githubusercontent.com/mcutshall/ansible_metrics/master/profile_
 # wget https://raw.githubusercontent.com/mcutshall/ansible_metrics/master/profile_tasks.py
 # chmod +x profile_tasks.py
 #
-# cd /opt/env/atmo_master/lib/python2.7/site-packages/subspace/plugins/callback/
-# rm -f profile_tasks.py  profile_tasks.pyc  profile_tasks.pyo
-# wget https://raw.githubusercontent.com/mcutshall/ansible_metrics/master/profile_tasks.py
-# python -m py_compile profile_tasks.py
+cd /opt/env/atmo_master/lib/python2.7/site-packages/subspace/plugins/callback/
+rm -f profile_tasks.py  profile_tasks.pyc  profile_tasks.pyo
+wget https://raw.githubusercontent.com/mcutshall/ansible_metrics/master/profile_tasks.py
+python -m py_compile profile_tasks.py
 #
 # cd /opt/dev/clank/callback_plugins
 # rm -f profile_tasks.py  profile_tasks.pyc
