3a725809bd74546b05b2c1e3c2d27b606264e5f5
mcutshall <cutshall@email.arizona.edu>
2018-07-09 11:30:01 -0700

    syntax

--- profile_tasks.py
+++ profile_tasks.py
@@ -120,7 +120,7 @@ class CallbackModule(CallbackBase):
             #params = config()
             #conn = psycopg2.connect("dbname=metrics, user=postgres, password=none")
             #conn = psycopg2.connect(**params)
-            conn = psycopg2.connect(host="127.0.0.1"database="metrics", user="postgres", password="none")
+            conn = psycopg2.connect(host="127.0.0.1", database="metrics", user="postgres", password="none")
             x = conn.cursor()
             print("xxxxxxxxxx connected to db xxxxxxxxxxxxxx")
         except:
