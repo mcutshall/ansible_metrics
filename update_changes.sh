439f797473701fc0e5acd3ed8554e80916f21d84
mcutshall <cutshall@email.arizona.edu>
2018-07-12 15:21:24 -0700

    updated bash update file

--- update_changes.sh
+++ update_changes.sh
@@ -3,11 +3,11 @@
 rm -f /opt/dev/atmosphere-ansible/ansible/playbooks/instance_deploy/callback_plugins/profile_tasks.py
 rm -f /opt/dev/atmosphere-ansible/ansible/playbooks/instance_deploy/callback_plugins/profile_tasks.pyc
 rm -f /opt/dev/atmosphere-ansible/ansible/playbooks/instance_deploy/callback_plugins/profile_tasks.pyo
-wget https://raw.githubusercontent.com/mcutshall/ansible_metrics/master/profile_tasks.py
+wget -P /opt/dev/atmosphere-ansible/ansible/playbooks/instance_deploy/callback_plugins/ https://raw.githubusercontent.com/mcutshall/ansible_metrics/master/profile_tasks.py
 python -m py_compile profile_tasks.py
 
 rm -f /opt/env/atmo_master/lib/python2.7/site-packages/subspace/plugins/callback/profile_tasks.py
 rm -f /opt/env/atmo_master/lib/python2.7/site-packages/subspace/plugins/callback/profile_tasks.pyc
 rm -f /opt/env/atmo_master/lib/python2.7/site-packages/subspace/plugins/callback/profile_tasks.pyo
-wget https://raw.githubusercontent.com/mcutshall/ansible_metrics/master/profile_tasks.py
+wget -P /opt/env/atmo_master/lib/python2.7/site-packages/subspace/plugins/callback/ https://raw.githubusercontent.com/mcutshall/ansible_metrics/master/profile_tasks.py
 python -m py_compile profile_tasks.py
