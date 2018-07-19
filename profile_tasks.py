c32f71e1945486ee7155543d7c03878c8e0772bc
mcutshall <cutshall@email.ariona.edu>
2018-07-19 11:00:07 -0700

    cleanup

--- profile_tasks.py
+++ profile_tasks.py
@@ -42,13 +42,6 @@ class CallbackModule(CallbackBase):
         if self.current is not None:
             self.stats[self.current] = time.time() - self.stats[self.current]
 
-        # Sort the tasks by their running time
-        results = sorted(
-            self.stats.items(),
-            key=lambda value: value[1],
-            reverse=True,
-        )
-
         conn = None
         x = None
         try:
